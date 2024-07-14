a51dceb059ba6ddf42081fdac40fad8e74718897
ingrid <ihannicht@gmail.com>
2024-07-14 09:00:31 -0400

    segunda revision realizada

--- javascripts/sale.js
+++ javascripts/sale.js
@@ -17,7 +17,7 @@ botonAgregar.addEventListener("click", e=> {
     carrito.push(listaProductos.value * cantidadProducto.value)
     console.log(carrito)
     // obtener la seleccion de los nombres y cantidades de cada producto
-    const selectedProduct = listaProductos.value;
+    const selectedProduct = listaProductos.options[listaProductos.selectedIndex].text;
     const selectedQuantity = cantidadProducto.value;
 
     // formato de los detalles del producto
@@ -32,16 +32,32 @@ botonTotal.addEventListener("click", e=> {
     let suma = 0
     let importetotal = 0
     let tarifaEnvio = 0
+    let detallesProductos = ""
 
     for (let indice = 0 ; indice < carrito.length ; indice++) {
-        suma += carrito [indice]
+        //obtener el precio del producto y la cantidad
+        const precioProducto = carrito[indice]; //los precios estan alojados en el array carrito
+        const cantidadProducto = 1; //cantidad es el array 1
+
+        //calcular el subtotal
+        const subtotalProducto = precioProducto * cantidadProducto;
+
+        //sumar el subtotal a la suma total
+        suma += subtotalProducto;
+
+        //obtener el nombre del producto 
+        const nombreProducto = listaProductos.options[indice].text;
+
+        //agg detalles del producto a la cadena 
+        detallesProductos += `\nProducto: ${nombreProducto} \nCantidad: ${cantidadProducto} unidades\nSubtotal: ${subtotalProducto} yenes\n`;
     }
+
+    //calcular el importe total
     console.log (suma)
     importetotal = suma //+ descuento (suma) 
     tarifaEnvio = descuento(suma)
     window.alert (`
-        La tarifa de envio es de ${tarifaEnvio} guaranies 
-        El importe total es de ${importetotal} guaranies
+        Detalles de la compra: \n${detallesProductos} \n\nLa tarifa de envio es de ${tarifaEnvio} yenes\nEl importe total es de ${importetotal} yenes
         `) //imprimir tarifa de envio y total 
     carrito = [] //vaciar el carrito
 })
